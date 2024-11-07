655d2c44d2587f27678c4d815905962c8a792a55
kerly.guanoquiza.7e8 <kerly.guanoquiza.7e8@itb.cat>
2024-11-07 12:49:33 +0100

    exercicis

--- JAVA SCRIPT/HelloName/HelloName.js
+++ JAVA SCRIPT/HelloName/HelloName.js
@@ -1,24 +1,9 @@
-
-const button = document.querySelector('button');
-const nameInput = document.getElementById('nameInput');
-const resultParagraph = document.getElementById('result');
-
-button.addEventListener('click', function() {
-
-    const name = nameInput.value;
-
-    resultParagraph.textContent = `Hello ${name}!`;
-});
-
-
-
-
 // resultado profe
 const button = document.querySelector("button");
 const input=document.querySelector("#nameInput");
 const result=document.querySelector("#result");
 function changeName(){
-    let name = input.valueOf()
-    result.textContent = `Hello ${name}`
+    let name = input.value
+    result.textContent = `Hello ${name}!`
 }
 button.addEventListener('click',changeName)
