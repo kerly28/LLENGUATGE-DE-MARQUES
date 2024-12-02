e5c974b24f8cd42d0f68edf870998718c3038348
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-12-02 23:02:47 +0100

    exercicis

--- WebEspeciesKerly/TORTUGA/Html/efectos.js
+++ WebEspeciesKerly/TORTUGA/Html/efectos.js
@@ -1,12 +1,33 @@
-// Esperamos que el DOM se cargue completamente
-document.addEventListener('DOMContentLoaded', function () {
-    // Obtenemos el botón por su id
-    const boton = document.getElementById('botonFondo');
 
-    // Añadimos el evento de clic al botón
-    boton.addEventListener('click', function () {
-        // Cambiamos la imagen de fondo del body
-        document.body.style.backgroundImage = "url('agua.jpg')";
+// efecte zoom imatges
+const images = document.querySelectorAll('.animal img');
+
+
+images.forEach(image => {
+    image.addEventListener('mouseover', function() {
+        image.style.transform = 'scale(1.2)';
+    });
+
+    image.addEventListener('mouseout', function() {
+        image.style.transform = 'scale(1)';
     });
 });
 
+
+//titol zoom
+
+document.addEventListener('DOMContentLoaded', function () {
+    const title = document.querySelector('#cap h1');
+
+    title.addEventListener('mouseenter', function () {
+        title.style.transform = 'scale(1.2)';
+        title.style.color = '#000000';
+        title.style.transition = 'all 0.3s ease';
+    });
+
+    title.addEventListener('mouseleave', function () {
+        title.style.transform = 'scale(1)';
+        title.style.color = 'limegreen';
+        title.style.textShadow = 'none'; // Elimina la sombra
+    });
+});
